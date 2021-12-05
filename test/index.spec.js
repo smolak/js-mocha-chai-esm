075e5f55bdf393ecacb99a5d9128c392a6789084
Jacek Smolak <jacek.smolak@gmail.com>
2021-12-05 21:51:31 +0100

    Run Prettier on files

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,8 +1,8 @@
 import { expect } from "chai";
-import { foo } from '../index';
+import { foo } from "../index";
 
-describe('foo function', () => {
-  it('should return bar', () => {
-    expect(foo()).to.equal('bar');
-  })
-})
+describe("foo function", () => {
+  it("should return bar", () => {
+    expect(foo()).to.equal("bar");
+  });
+});
